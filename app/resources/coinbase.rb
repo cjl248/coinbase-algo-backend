c039da527c5ddd903d91b09b03ef6c6bbebb33db
github cjl248 <github cjl248@cornell.edu>
2020-12-30 17:07:17 -0500

    market_order method working as expected to succesfully execute market buy/sell orders.

--- app/resources/coinbase.rb
+++ app/resources/coinbase.rb
@@ -6,9 +6,8 @@ require 'pry'
 
 class Coinbase
 
-  API = 'https://api.pro.coinbase.com'
-
   def initialize()
+    @API = 'https://api.pro.coinbase.com'
     @key = ENV['API_KEY']
     @secret = ENV['API_SECRET']
     @passphrase = ENV['PASSPHRASE']
@@ -21,9 +20,7 @@ class Coinbase
   def signature(request_path='', body='', timestamp=nil, method='')
     body = body.to_json if body && body.is_a?(Hash)
     timestamp = Time.now.utc.to_i if !timestamp
-
     message = "#{timestamp}#{method}#{request_path}#{body}"
-
     # create a sha256 hmac with the secret
     secret = Base64.decode64(@secret)
     hash  = OpenSSL::HMAC.digest('sha256', secret, message)
@@ -33,13 +30,13 @@ class Coinbase
   def get_account(id='', ledger=false)
     timestamp = Time.now.utc.to_i
     method = 'GET'
+    request_path = "/accounts/#{id}" if ledger == false
     request_path = "/accounts/#{id}/ledger"if ledger == true
-    request_path = "/accounts/#{id}"
     body = ''
     sign = signature(request_path, body, timestamp, method)
     response = RestClient.get(
-      API+request_path,
-      headers=  {
+      @API+request_path,
+      headers =  {
         'Content-Type': 'application/json',
         'CB-ACCESS-KEY': @key,
         'CB-ACCESS-SIGN': sign,
@@ -47,6 +44,56 @@ class Coinbase
         'CB-ACCESS-PASSPHRASE': @passphrase
       })
     response
+  end
+
+  # side = buy/sell
+  # product_id="BTC-USD", "XLM-USD"
+  # size[optional] = desired ammount in base currency (BTC)
+  # funds[optional] = desired ammount of quote currency ($)
+  def market_order(side, product_id, size, funds)
+
+    timestamp = Time.now.utc.to_i
+    method = 'POST'
+    request_path = "/orders"
+
+    body = {
+      "type": "market",
+      "side": side,
+      "size": size,
+      "product_id": product_id
+    } if !!size
+    body = {
+      "type": "market",
+      "side":  side,
+      "funds": funds,
+      "product_id": product_id
+    } if !!funds
+
+    sign = signature(request_path, body, timestamp, method)
+
+    headers = {
+      'Content-Type': 'application/json',
+      'CB-ACCESS-KEY': @key,
+      'CB-ACCESS-SIGN': sign,
+      'CB-ACCESS-TIMESTAMP': timestamp,
+      'CB-ACCESS-PASSPHRASE': @passphrase
+    }
+    
+    response = RestClient.post(
+      @API+request_path,
+      body.to_json,
+      headers
+    )
+
+    response
+  end
+
+  # side = buy/sell
+  # product_id="BTC-USD", "XLM-USD"
+  def limit_order(type='limit', side, product_id, price, size)
+    timestamp = Time.now.utc.to_i
+    method = 'POST'
+    request_path = "/orders"
   end
 
 end
