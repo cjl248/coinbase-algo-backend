f1a8c2bf6ae088109583e231599b05f9f6a08cbe
github cjl248 <github cjl248@cornell.edu>
2021-01-02 11:17:59 -0500

    limit_order working as expected

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,12 +3,12 @@ require 'coinbase.rb'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#
+
 
 request = Coinbase.new
+
 
 # GET /accounts
 #
@@ -23,11 +23,46 @@ request = Coinbase.new
 #   return err.response
 # end
 
-# POST /orders
+
+# GET /orders
+#
+# begin
+#   response = Coinbase.parse_response(
+#     request.get_orders()
+#   )
+#   binding.pry
+#   response
+# rescue RestClient::BadRequest => err
+#   binding.pry
+#   return err.response
+# rescue RestClient::Unauthorized, RestClient::Forbidden => err
+#   binding.pry
+#   return err.response
+# end
+
+
+# POST /orders - market_order
+#
+# begin
+#   response = Coinbase.parse_response(
+#     request.market_order('buy', 'XLM-USD', false, "0.07")
+#   )
+#   binding.pry
+#   response
+# rescue RestClient::BadRequest => err
+#   binding.pry
+#   return err.response
+# rescue RestClient::Unauthorized, RestClient::Forbidden => err
+#   binding.pry
+#   return err.response
+# end
+
+
+# POST /orders - limit_order
 #
 begin
   response = Coinbase.parse_response(
-    request.market_order('buy', 'XLM-USD', false, "0.07")
+    request.limit_order('GTT', 'day', 'buy', 'BTC-USD', '31250.00', '0.00238001')
   )
   binding.pry
   response
